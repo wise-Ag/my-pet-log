deefe6183c2d146c73d21a790a9619610ad209c6
Hyeyoon <foryou9097@gmail.com>
2024-03-29 23:34:41 +0900

    [#313] feat : 구독중인 펫계정 숫자 표시되도록 하기

--- app/settings/subscriptions/page.tsx
+++ app/settings/subscriptions/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import SubscriptionList from "../_components/SubscriptionList";
 import TitleHeader from "@/app/_components/TitleHeader";
+import { getSubscribedPet } from "@/app/_api/subscription";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { SubscribedPetType } from "@/app/_types/subscriptions/types";
 
 const Page = () => {
+  const queryClient = useQueryClient();
+  const { data: accountsData } = useQuery<SubscribedPetType[]>({
+    queryKey: ["subscribedPet"],
+    queryFn: () => getSubscribedPet(),
+  });
+
   return (
     <div>
-      <TitleHeader title="구독중인 펫 계정 (2)" />
+      <TitleHeader title={`구독중인 펫 계정 (${accountsData ? accountsData.length : 0})`} />
       <SubscriptionList />
     </div>
   );
