261a1aac78319f43555e80f46fc558a35ad0209b
손지은 <wiseag@naver.com>
2024-04-03 23:58:53 +0900

    [#318] refactor : 일기 수정 이미지 다루는 법 변경

--- app/diary/_components/Input/ImageInput/index.tsx
+++ app/diary/_components/Input/ImageInput/index.tsx
@@ -1,15 +1,15 @@
 import Modal from "@/app/_components/Modal";
 import { useModal } from "@/app/_hooks/useModal";
+import { diaryImagesAtom } from "@/app/_states/atom";
+import { DiaryMediaType } from "@/app/_types/diary/type";
+import { FormInput } from "@/app/diary/_components/Form/EditForm";
 import { DragDropContext, Draggable, DropResult, Droppable } from "@hello-pangea/dnd";
-import { useEffect, useState } from "react";
-import { FieldValues, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { IoIosCloseCircle } from "react-icons/io";
 import { LuImageOff, LuImagePlus } from "react-icons/lu";
 import * as styles from "./style.css";
-import { useAtom } from "jotai";
-import { deletedImagesAtom, diaryImagesAtom } from "@/app/_states/atom";
-import { FormInput } from "@/app/diary/_components/Form/EditForm";
-import { DiaryMediaType } from "@/app/_types/diary/type";
 
 export interface ImagesType {
   name: string;
@@ -23,15 +23,23 @@ export interface InputProps {
   setValue: UseFormSetValue<FormInput>;
   oldMedia?: DiaryMediaType[];
 }
+const convertURLtoFile = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.blob();
+  const filename = url.split("/").pop(); // url 구조에 맞게 수정할 것
+  const metadata = { type: `image/jpeg` };
+  return new File([data], filename!, metadata);
+};
 
 const ImageInput = ({ register, setValue, oldMedia }: InputProps) => {
   const [images, setImages] = useAtom(diaryImagesAtom);
-  const [, setDeletedImages] = useAtom(deletedImagesAtom);
-  const [oldData, setOldData] = useState(oldMedia);
 
   useEffect(() => {
     //edit용 이전 데이터
-    setOldData(oldMedia);
+    oldMedia?.forEach(async (v) => {
+      const file = await convertURLtoFile(process.env.NEXT_PUBLIC_IMAGE_PREFIX + v.path);
+      setImages((prev) => [...prev, { name: file.name, file: file, url: URL.createObjectURL(file) }]);
+    });
   }, [oldMedia]);
 
   const { isModalOpen, openModalFunc, closeModalFunc } = useModal();
@@ -39,13 +47,12 @@ const ImageInput = ({ register, setValue, oldMedia }: InputProps) => {
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (!files) return;
-    if (files?.length + images.length + (oldData ? oldData?.length : 0) > MAX_IMAGES) {
+    if (files?.length + images.length > MAX_IMAGES) {
       openModalFunc();
       return;
     }
     for (let i = 0; i < files?.length; i++) {
       if (images.filter((v) => v.name === files[i].name).length > 0) break; //같은 이미지는 무시함
-
       setImages((prev) => [...prev, { name: files[i].name, file: files[i], url: URL.createObjectURL(files[i]) }]);
     }
   };
@@ -55,10 +62,6 @@ const ImageInput = ({ register, setValue, oldMedia }: InputProps) => {
     setValue("images", null);
   };
 
-  const deleteOldImage = (mediaId: number) => {
-    setOldData((prev) => prev?.filter((v) => v.mediaId !== mediaId));
-    setDeletedImages((prev) => [...prev, mediaId]);
-  };
   const onDragEnd = (result: DropResult) => {
     const { destination, source } = result;
     if (!destination) return; //드래그앤드랍 컨테이너 벗어났을 때
@@ -86,23 +89,13 @@ const ImageInput = ({ register, setValue, oldMedia }: InputProps) => {
             onChange={handleImageChange}
             // disabled={images.length >= MAX_IMAGES}
           />
-          <label htmlFor="images" className={`${styles.input} ${images.length + (oldData ? oldData.length : 0) >= MAX_IMAGES && styles.disabledInput}`}>
-            {images.length + (oldData ? oldData.length : 0) >= MAX_IMAGES ? <LuImageOff className={styles.addIcon} /> : <LuImagePlus className={styles.addIcon} />}
+          <label htmlFor="images" className={`${styles.input} ${images.length >= MAX_IMAGES && styles.disabledInput}`}>
+            {images.length >= MAX_IMAGES ? <LuImageOff className={styles.addIcon} /> : <LuImagePlus className={styles.addIcon} />}
           </label>
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="one" direction="horizontal">
               {(provided) => (
                 <div className={styles.container} {...provided.droppableProps} ref={provided.innerRef}>
-                  {oldData &&
-                    oldData.map((v) => {
-                      return (
-                        <div key={v.mediaId}>
-                          <div className={styles.preview} style={{ backgroundImage: `url(${process.env.NEXT_PUBLIC_IMAGE_PREFIX}${v.path})` }}>
-                            <IoIosCloseCircle className={styles.closeIcon} onClick={() => deleteOldImage(v.mediaId)} />
-                          </div>
-                        </div>
-                      );
-                    })}
                   {images.map((image, index) => {
                     return (
                       <Draggable draggableId={String(image.name)} index={index} key={image.name}>
@@ -123,7 +116,7 @@ const ImageInput = ({ register, setValue, oldMedia }: InputProps) => {
           </DragDropContext>
         </div>
         <p className={styles.p}>
-          이미지 최대 {MAX_IMAGES}개 중 {images.length + (oldData ? oldData.length : 0)}개
+          이미지 최대 {MAX_IMAGES}개 중 {images.length}개
         </p>
         {isModalOpen && <Modal text="최대 이미지 수 10개를 초과했습니다." onClick={closeModalFunc} buttonText="확인" onClose={closeModalFunc} />}
       </div>
