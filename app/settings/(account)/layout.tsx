86e69b722b4141a26f48e0b5d7c018f95df670d7
HyeyoonS <foryou9097@gmail.com>
2024-05-13 19:45:05 +0900

    [#391] feat : 마이프로필 에서 loginType에 따라 비밀번호 변경페이지 진입가능여부 판단

--- app/settings/(account)/layout.tsx
+++ app/settings/(account)/layout.tsx
@@ -4,9 +4,24 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import * as styles from "@/app/settings/(account)/layout.css";
 import TitleHeader from "@/app/_components/TitleHeader";
+import { getMe } from "@/app/_api/users";
+import { useQuery } from "@tanstack/react-query";
+import { UserType } from "@/app/_types/user/types";
+import { showToast } from "@/app/_components/Toast";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const { data: user } = useQuery<UserType>({
+    queryKey: ["me"],
+    queryFn: () => getMe(),
+  });
+
+  const isEmailLogin = user?.loginType === "EMAIL";
+
+  const handleUnauthorized = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    showToast("이메일로 가입한 회원만 접근할 수 있습니다.", false);
+  };
 
   return (
     <section>
@@ -18,10 +33,16 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
               프로필 설정
             </Link>
           </li>
-          <li className={`${pathname === "/settings/password" ? styles.active : styles.noActive}`}>
-            <Link href="/settings/password" replace>
-              비밀번호 변경
-            </Link>
+          <li className={`${isEmailLogin && pathname === "/settings/password" ? styles.active : styles.noActive}`}>
+            {isEmailLogin ? (
+              <Link href="/settings/password" replace>
+                <a>비밀번호 변경</a>
+              </Link>
+            ) : (
+              <Link href="#" onClick={handleUnauthorized}>
+                비밀번호 변경
+              </Link>
+            )}
           </li>
         </ul>
       </nav>
