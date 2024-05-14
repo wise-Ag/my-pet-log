2b4c85a41510511578c0729cd615c4565a7a5e6d
Hayden <hayden.dev.k@gmail.com>
2024-05-14 15:55:28 +0900

    [#280] refactor : 이미지 포맷 적용

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     return config;
   },
   images: {
-    // formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
